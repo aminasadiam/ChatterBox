ec5a1021c27ed44da06fb989c4fedd5a4da8554b
Amin <aminasadiam@outlook.com>
2023-08-11 17:45:47 +0330

    install mux and jwt package

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/aminasadiam/ChatterBox
 
 go 1.20
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/crypto v0.12.0
+)
--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"log"
 
 	"github.com/aminasadiam/ChatterBox/datalayer"
+	"github.com/aminasadiam/ChatterBox/restapi"
 )
 
 func main() {
 	db, err := datalayer.CreateDbConnection("")
 	if err != nil {
 		log.Fatalln(err)
+		return
+	}
+
+	err = restapi.RunApi("localhost:80", *db)
+	if err != nil {
+		log.Println(err)
 		return
 	}
 }
